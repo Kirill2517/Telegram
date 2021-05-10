f0ce1fe264efd1390b038b1d54715c721e924633
Kirille <kirillmakarichev1@gmail.com>
2021-05-10 14:39:44 +0300

    no message

---
+++ TelegramLib/Share/DebugM/Managers/DebugManager.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramLib.Share.Models;
+
+namespace TelegramLib.Share.Debug.Managers
+{
+    public class DebugManager : DataBaseController
+    {
+        public DebugManager(MySqlConnection mySqlConnection) : base(mySqlConnection)
+        {
+        }
+       
+        public async Task DeleteRecords()
+        {
+            await this.ActionCommand("call delete_all_data();");
+        }
+    }
+}
